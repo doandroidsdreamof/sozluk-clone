85ae75b78b413b23de41ce90f6d399fee0b3e2bc
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-06-12 02:36:26 +0300

    break-words bug fixed

--- src/components/modules/topic/TopicHeader.tsx
+++ src/components/modules/topic/TopicHeader.tsx
@@ -7,8 +7,8 @@ interface TopicHeaderProps {
 
 const TopicHeader = ({ headerOne, headerTwo }: TopicHeaderProps) => {
   return (
-    <header className=" mb-4 h-fit  w-full lg:mb-6">
-      <h1 className="mb-4 text-3xl font-bold leading-tight text-typography-body-light dark:text-typography-body-dark lg:mb-6 lg:text-4xl">
+    <header className=" mb-4 h-fit   w-full lg:mb-6">
+      <h1 className="mb-4 max-w-4xl break-words text-3xl font-bold leading-tight text-typography-body-light dark:text-typography-body-dark lg:mb-6 lg:text-4xl">
         {headerOne}
       </h1>
       <h2 className=" text-lg leading-tight text-typography-body-secondary-dark dark:text-typography-body-secondary-dark lg:mb-6 lg:text-2xl">
