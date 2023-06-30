2923b9db077f438fb4bfd6df36e4fe3127d677f9
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2023-07-01 00:50:30 +0300

    profile links

--- src/components/modules/profile/ProfileCard.tsx
+++ src/components/modules/profile/ProfileCard.tsx
@@ -1,5 +1,6 @@
 import Avatar from "~/components/common/Avatar";
 import { useState } from "react";
+import Link from "next/link";
 
 interface ProfileCardProps {
   imageURL: string;
@@ -9,21 +10,25 @@ interface ProfileCardProps {
 
 const ProfileCard = ({ imageURL, date, name }: ProfileCardProps) => {
   return (
-    <div className="ml-auto flex flex-row">
+    <div className="ml-auto  flex flex-row">
       <div className="mt-0.5">
-        <h1 className="text-[0.80rem]   font-bold text-typography-body-light dark:text-typography-body-dark">
-          {name}
-        </h1>
-        <p className="font-helvetica text-[0.75em] font-light text-typography-body-secondary-light dark:text-typography-body-faded-light">
+        <Link href={`/profile/${name}`}>
+          <h1 className="cursor-pointer text-[0.80rem] font-bold  text-typography-body-light hover:underline dark:text-typography-body-dark">
+            {name}
+          </h1>
+        </Link>
+        <p className="font-helvetica text-[0.75em]  font-light text-typography-body-secondary-light dark:text-typography-body-faded-light">
           {date?.toDateString()}
         </p>
       </div>
-      <Avatar
-        style="mx-4 block  h-10 w-10 cursor-pointer  rounded-full object-cover"
-        alt="avatar"
-        src="/images/default-avatar.png"
-        fallbackSrc="/images/default-avatar.png"
-      />
+      <Link href={`/profile/${name}`}>
+        <Avatar
+          style="mx-4 block  h-10 w-10 cursor-pointer  rounded-full object-cover"
+          alt="avatar"
+          src="/images/default-avatar.png"
+          fallbackSrc="/images/default-avatar.png"
+        />
+      </Link>
     </div>
   );
 };
