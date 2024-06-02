10ab0fb4368dbffb4bbf7f9dd6e5ecce37efe4d5
doandroidsdreamof <mutlukuytuoglu@gmail.com>
2024-06-02 22:52:05 +0300

    husky skip install && meta tag removed

--- src/components/common/DocHead.tsx
+++ src/components/common/DocHead.tsx
@@ -4,7 +4,6 @@ import { META_TEXT } from "@/constants/staticContents";
 const DocHead = () => {
   return (
     <Head>
-      <meta name="description" />
       <title>{META_TEXT.TITLE}</title>
       <link rel="shortcut icon" href="favicon.ico" />
     </Head>
